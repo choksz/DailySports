1185fee7ec05fc9508beb8629874b3b41d391d85
Pawel Kapica <pawel.kapica@gmail.com>
2016-11-24 23:03:07 +0100

    small fix for nullreference

--- src/DailySports.Backend/Program.cs
+++ src/DailySports.Backend/Program.cs
@@ -16,6 +16,7 @@ namespace DailySports.Backend
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+					 .UseUrls("http://localhost:5001/")
                 .Build();
 
             host.Run();
